2bd53227842655994a22147bc9ab873cb387436d
Jing <qinj1994@163.com>
2018-08-29 09:14:37 +0800

    Add files via upload

--- findPrime.java
+++ findPrime.java
@@ -1,112 +1,89 @@
 import java.util.Scanner;
 
 public class findPrime {
-
-	public static void main(String[] args) {
-//		// loop n times
-		Scanner in = new Scanner(System.in);
-//		int x = in.nextInt();
-//		boolean isPrime = true;
-//		if (x == 1||x==2) {
-//			isPrime = false;
-//		}
-//		for(int i=2;i<x;i++) {
-//			if(x%i == 0) {
-//				isPrime = false;
-//				break;
-//			}
-//		}
-//		if (isPrime) {
-//			System.out.println(x+" is a Prime");
-//		}
-//		else {
-//			System.out.println(x+" is not a Prime");
-//		}
-//		
-//		// loop for n/2
-//		if(x == 1||x%2 == 0&&x!=2) {
-//			isPrime = false;
-//		}
-//		else {
-//			for(int j=3;j<x;j=j+2) {
-//				if(x%j == 0) {
-//					isPrime = false;
-//					break;
-//				}
-//			}
-//		}
-//		if (isPrime) {
-//			System.out.println(x+" is a Prime");
-//		}
-//		else {
-//			System.out.println(x+" is not a Prime");
-//		}
-//		
-//		//loop for sqrt(n)
-//		if(x == 1||x%2 == 0&&x!=2) {
-//			isPrime = false;
-//		}
-//		else {
-//			for(int j=3;j<Math.sqrt(x);j=j+2) {
-//				if(x%j == 0) {
-//					isPrime = false;
-//					break;
-//				}
-//			}
-//		}
-//		if (isPrime) {
-//			System.out.println(x+" is a Prime");
-//		}
-//		else {
-//			System.out.println(x+" is not a Prime");
-//		}
-		
-		// another way is to build a 50 Primes Array
-		// to find if the x%Primenum == 0
-		
-//		int[] Primes = new int[50];
-//		Primes[0] = 2;
-//		int count = 1;
-//		Main_loop:
-//		for(int x=3;count<50;x++) {
-//			for(int i=0;i<count;i++) {
-//				if(x%Primes[i] == 0) {
-//					continue Main_loop;
-//				}
-//			}
-//			// Primes[count] = x;
-//			// count = count + 1;
-//			Primes[count++] = x;
-//		}
-//		for(int m:Primes) {
-//			System.out.print(m+ " ");
-//		}
-//		System.out.println(Primes.length);	
-		
-		boolean[] haha = new boolean[100];
-		for(int i=0;i<haha.length;i++) {
-			haha[i] = true;
-		}
-		
-		haha[0] = false;
-		haha[1] = false;
-		for(int j=2;j<haha.length;j++) 
+	public static boolean isPrime(int n)
+	{
+		boolean isP = true;
+		for(int i=2;i<n;i++)
 		{
-			if(haha[j]) 
+			if(n%i == 0)
 			{
-				for(int m=2;m*j<haha.length;m++) 
-				{
-					haha[m*j] = false;
-				}
-			}
-		}
-		for(int n=2;n<haha.length;n++) 
-		{
-			if(haha[n]) 
-			{
-				System.out.print(n+" ");
+				isP = false;
+				break;
 			}
 			
 		}
+		return isP;
 	}
+	
+	public static int ArrMul(int[] a)
+	{
+		int m = 1;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]!=0)
+			{
+				m = m*a[i];
+			}			
+		}
+		return m;
+	}
+	
+	public static int[] ArrPrime(int n)
+	{
+		int[] Arr = new int[n];
+		int cnt = 0;
+		for(int i=2;i<=n;)
+		{
+			if(isPrime(i)&&n%i == 0)
+			{
+				Arr[cnt] = i;
+				cnt = cnt + 1;
+				n = n/i;
+			}
+			else
+			{
+				i = i + 1;
+			}
+		}
+		return Arr;
+	}
+
+	public static void main(String[] args) {
+//		System.out.println(isPrime(15));
+//		
+//		int[] b = {1,2,0};
+//		
+//		System.out.println(ArrMul(b));
+		
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		int[] arr = ArrPrime(num);
+		int cnt = 0;
+		if(isPrime(num))
+		{
+			System.out.println(num+"="+num);
+		}
+		else
+		{
+			System.out.print(num+"=");
+			for(int i=0;i<arr.length;i++)
+			{
+				if(cnt == 0)
+				{
+					System.out.print(arr[i]);
+					cnt = cnt + 1;
+					continue;
+				}
+				
+				if(arr[i]!=0)
+				{
+					System.out.print("x"+arr[i]);
+				}		
+				
+			}
+		}
+
+	}
+
 }
